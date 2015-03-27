f0b0654fb536af4aa44faa670b72ba5d2bd6d0b3
Christian Foellmann <foellmann@foe-services.de>
2015-03-27 09:25:44 +0100

    extend loader to look in 'AdminSDK' directory for files

--- loader.php
+++ loader.php
@@ -6,14 +6,34 @@ Version:     0.0.7-dev
  */
 
 function AdminSDK_require_files() {
-	/** PageAPI: Create admin pages */
-	require_once 'PageAPI.php';
 
-	/** SettingsAPI: Create admin settings pages - Extends PageAPI*/
-	require_once 'SettingsAPI.php';
+	$path   = dirname( __FILE__ ) . DIRECTORY_SEPARATOR;
+	$folder = 'AdminSDK' . DIRECTORY_SEPARATOR;
 
-	/** Utils: Collection of utility functions */
-	require_once 'Utils.php';
-}
+	if ( file_exists( $path . 'PageAPI.php') ) {
+
+		/** PageAPI: Create admin pages */
+		require_once $path . 'PageAPI.php';
+
+		/** SettingsAPI: Create admin settings pages - Extends PageAPI*/
+		require_once $path . 'SettingsAPI.php';
+
+		/** Utils: Collection of utility functions */
+		require_once $path . 'Utils.php';
+
+	} elseif ( file_exists( $path . $folder . 'PageAPI.php') ) {
+
+		/** PageAPI: Create admin pages */
+		require_once $path . $folder . 'PageAPI.php';
+
+		/** SettingsAPI: Create admin settings pages - Extends PageAPI*/
+		require_once $path . $folder . 'SettingsAPI.php';
+
+		/** Utils: Collection of utility functions */
+		require_once $path . $folder . 'Utils.php';
+
+	}
+
+} // END AdminSDK_require_files()
 
 add_action( 'plugins_loaded', 'AdminSDK_require_files', 1 );
